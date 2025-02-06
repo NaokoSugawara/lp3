8996576d475782773c77e8bd5bbafccd30579317
naoko.sugawara <infoseek122@gmail.com>
2025-02-06 10:52:48 -0500

    removed unnecessary comments

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,19 +1,3 @@
-console.log("Script loaded!");
-
-
-
-// import Splide from '@splidejs/splide';
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   new Splide('.splide', {
-//     type: 'loop',   // Enables infinite scrolling
-//     perPage: 3,     // Number of slides visible per page
-//     autoplay: true, // Auto slides transition
-//     // width: 80%,
-//     // padding: {left:100, right:100}
-    
-//   }).mount();
-// });
 
 document.addEventListener('DOMContentLoaded', function () {
   new Splide('.splide', {
